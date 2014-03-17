9fc619019959d98bff52dc4514011c6ab18534f7
Nathan Caza <mastercactapus@gmail.com>
2014-03-16 19:03:46 -0500

    - usage monitoring - process tree support - instant reload - split restart/reload - per-app details/status

--- bin/napps.js
+++ bin/napps.js
@@ -15,6 +15,7 @@ var moment = require("moment");
 var daemonsocket = path.join(napp.basedir, "daemon.socket");
 
 
+//add new apps
 cli.command("add <scriptfile> [name]")
 .option("--start", "start the application immediately")
 .option("--cwd <dir>", "set working directory [default: current]", process.cwd())
@@ -39,33 +40,112 @@ cli.command("add <scriptfile> [name]")
 	.done();
 });
 
-cli.command("restart <name>")
-.description("Gracefully restart an application (--force to skip shutdown)")
-.option("--force", "Restart without sending shutdown signal")
-.action(function(name, opts){
-	ensureDaemon(true)
-	.then(daemonRequest.bind(null, "/apps/" + name + "/" + (opts.force ? "restart" : "reload"), null, "POST"))
-	.then(allStatus)
-	.done();
-});
 
-cli.command("start <name>")
-.description("Start an application")
-.action(function(name){
-	ensureDaemon(true)
-	.then(daemonRequest.bind(null, "/apps/" + name + "/start", null, "POST"))
-	.then(allStatus)
-	.done();
-});
 
-cli.command("stop <name>")
-.description("Stop a running application")
+
+
+cli.command("start *")
+.usage("[options] [NAME...]")
+.description("Start application(s)")
+.option("--all", "Apply to all applications (ignores names)")
 .action(function(name){
-	ensureDaemon(true)
-	.then(daemonRequest.bind(null, "/apps/" + name + "/stop", null, "POST"))
+
+	var names = [].slice.call(arguments, 0, -1);
+	var opts = [].slice.call(arguments, -1)[0];
+	if (opts.all) names = getNames();
+
+	sequence(names, SSRApp.bind(null, "start"))
 	.then(allStatus)
 	.done();
 });
+cli.command("stop *")
+.usage("[options] [NAME...]")
+.description("Stop running application(s)")
+.option("--all", "Apply to all applications (ignores names)")
+.action(function(){
+
+	var names = [].slice.call(arguments, 0, -1);
+	var opts = [].slice.call(arguments, -1)[0];
+	if (opts.all) names = getNames();
+
+	sequence(names, SSRApp.bind(null, "stop"))
+	.then(allStatus)
+	.done();
+});
+cli.command("restart *")
+.usage("[options] [NAME...]")
+.description("Restart applications (including cluster process)")
+.option("--all", "Apply to all applications (ignores names)")
+.action(function(){
+
+	var names = [].slice.call(arguments, 0, -1);
+	var opts = [].slice.call(arguments, -1)[0];
+	if (opts.all) names = getNames();
+
+	sequence(names, SSRApp.bind(null, "restart"))
+	.then(allStatus)
+	.done();
+});
+cli.command("reload *")
+.usage("[options] [NAME...]")
+.description("Gracefully restart an application(s)")
+.option("--instant", "Replace all instances immediately instead of one-by-one (uses more resources)")
+.option("--all", "Apply to all applications (ignores names)")
+.action(function(){
+
+	var names = [].slice.call(arguments, 0, -1);
+	var opts = [].slice.call(arguments, -1)[0];
+	if (opts.all) names = getNames();
+
+	sequence(names, SSRApp.bind(null, "reload?instant=" + (opts.instant||"")))
+	.then(allStatus)
+	.done();
+});
+cli.command("delete *")
+.usage("[options] [NAME...]")
+.description("Stop and delete application(s)")
+.option("--all", "Apply to all applications (ignores names)")
+.action(function(){
+
+	var names = [].slice.call(arguments, 0, -1);
+	var opts = [].slice.call(arguments, -1)[0];
+
+	if (opts.all) names = getNames();
+
+	sequence(names, SSRApp.bind(null, "delete"))
+	.then(allStatus)
+	.done();
+});
+function sequence(params, fn) {
+	return Q.all([params, fn])
+	.spread(function(params, fn){
+		var chain = Q();
+		params.forEach(function(params){
+			chain = chain.then(fn.bind(null, params));
+		});
+		return chain;
+	});
+}
+function SSRApp(method, appname) {
+	var httpMethod = "POST";
+	var methodName = method.split("?")[0] + ":";
+	if (method === "delete") {
+		method = "";
+		httpMethod = "DELETE";
+	} else {
+		method = "/" + method;
+	}
+	return ensureDaemon(true)
+	.then(function(){
+		console.log(methodName, appname);
+	})
+	.then(daemonRequest.bind(null, "/apps/" + appname + method, null, httpMethod))
+	.catch(function(err){
+		console.log(methodName, appname, "FAILED", err.message||err);
+	});
+}
+
+
 
 cli.command("logs [name[:workerId]]")
 .description("View streaming logs from running applications, or just one")
@@ -81,8 +161,24 @@ cli.command("logs [name[:workerId]]")
 	logs.pipe(process.stdout); 
 });
 
+
 cli.command("status [name]")
 .description("View application status")
+.action(function(name){
+	appStatus(name).done();
+});
+cli.command("list [name]")
+.description("alias: status")
+.action(function(name){
+	appStatus(name).done();
+});
+cli.command("ls [name]")
+.description("alias: status")
+.action(function(name){
+	appStatus(name).done();
+});
+cli.command("ps [name]")
+.description("alias: status")
 .action(function(name){
 	appStatus(name).done();
 });
@@ -101,7 +197,6 @@ cli.command("stop-daemon")
 	})
 	.done();
 });
-
 cli.command("start-daemon")
 .description("Starts the daemon process if not running")
 .action(function(){
@@ -115,7 +210,6 @@ cli.command("start-daemon")
 	})
 	.done();
 });
-
 cli.command("restart-daemon")
 .description("Restarts the daemon process")
 .action(function(){
@@ -125,7 +219,14 @@ cli.command("restart-daemon")
 });
 
 
+cli.command("help")
+.description("output usage information")
+.action(function(command){
+	cli.help();
+});
+
 cli.parse(process.argv);
+
 
 function addApp(config, start) {
 	return ensureDaemon(true)
@@ -200,16 +301,17 @@ function daemonRequest(uri, data, method) {
 		json: data||true
 	})
 	.spread(function(res, body){
-		if (res.statusCode === 200) {
+		if (res.statusCode >= 200 && res.statusCode < 300) {
 			return body;
-		} else if (res.statusCode > 299) {
-			throw new Error("ERR: " + res.statusCode + "\n" + body);
 		} else {
-			return res.statusCode;
+			throw new Error(res.statusCode + ": " + body);
 		}
 	});
 }
-
+function getNames() {
+	return ensureDaemon(true)
+	.then(daemonRequest.bind(null, "/apps?namesOnly=true"))
+}
 function allStatus() {
 	return appStatus();
 }
@@ -223,39 +325,104 @@ function appStatus(name) {
 function printStatus(details) {
 	if (!_.isArray(details)) details = [details];
 
-	var tableHeader = ["App Name", "Status", "PID", "Uptime", "Crashes", "Ports", "#", "Instance PIDs"];
+	var tableHeader = ["App Name", "Status", "PID", "Uptime", "Crashes", "Ports", "#", "CPU", "MEM"];
 	tableHeader = tableHeader.map(function(header){
 		return header.cyan.bold;
-	})
+	});
 
 	var table = new Table({
 		head: tableHeader
 	});
 
-	_.each(details, function(app){
-		var status = "OFFLINE".red;
-		var instances = "", ports = "", uptime = "", workerPIDs = "", pid = "", crashes = "";
-		if (app.status) {
-			if (app.status.status === "ONLINE") {
-				status = "ONLINE".green;
-			} else {
-				status = app.status.status.yellow;
+	if (details.length > 1) {
+		_.each(details, function(app){
+			var status = "OFFLINE".red;
+			var instances = "", ports = "", uptime = "", workerPIDs = "", pid = "", crashes = "", cpu = "", mem = "";
+			if (app.status) {
+                makeTotals(app);
+				if (app.status.status === "ONLINE") {
+					status = "ONLINE".green;
+				} else {
+					status = app.status.status.yellow;
+				}
+
+				instances = Object.keys(app.status.workers).length;
+
+				ports = _.flatten(app.status.workers, "listening");
+				ports = _.uniq(ports, "port");
+				ports = _.pluck(ports, "port");
+				ports = ports.join(" ");
+				uptime = moment.duration(app.status.uptime, "seconds").humanize();
+				pid = app.status.pid;
+				crashes = app.status.deathCount - app.status.killCount;
+                cpu = app.status.totalCpu.toFixed(2) + "%";
+                mem = prettyMem(app.status.totalMem);
 			}
 
-			instances = Object.keys(app.status.workers).length;
+			table.push([app.id.bold, status, pid, uptime, crashes, ports, instances, cpu, mem]);
+		});
 
-			ports = _.flatten(app.status.workers, "listening");
-			ports = _.uniq(ports, "port");
-			ports = _.pluck(ports, "port");
-			ports = ports.join(" ");
-			uptime = moment.duration(app.status.uptime, "seconds").humanize();
-			pid = app.status.pid;
-			workerPIDs = _.pluck(app.status.workers, "pid").join(" ");
-			crashes = app.status.deathCount - app.status.killCount;
-		}
+		console.log(table.toString());
+	} else if (details.length === 1) {
+		printDetailedStatus(details[0]);
+	} else {
+		console.log("No apps currently configured");
+	}
 
-		table.push([app.id.bold, status, pid, uptime, crashes, ports, instances, workerPIDs]);
+}
+
+function prettyMem(bytes) {
+    var labels = ["b", "KiB", "MiB", "GiB", "TiB"];
+    for (var i = labels.length-1;i>=0;i--) {
+        if (bytes > Math.pow(1000, i)) {
+            return (bytes/Math.pow(1024, i)).toFixed(2) + " " + labels[i];
+        }
+    }
+    
+    return bytes + " " + labels[0];
+}
+
+function makeTotals(app) {
+    if (app.status) {
+        app.status.totalProc = 1;
+        app.status.totalMem = app.status.mem;
+        app.status.totalCpu = app.status.cpu;
+        _.each(app.status.workers, function(worker){
+            worker.totalProc = 1 + worker.children.length;
+            worker.totalMem = worker.mem;
+            worker.totalCpu = worker.cpu;
+            
+            _.each(worker.children, function(child){
+                worker.totalMem += child.mem;
+                worker.totalCpu += child.cpu;
+            });
+            
+            app.status.totalProc += worker.totalProc;
+            app.status.totalMem += worker.totalMem;
+            app.status.totalCpu += worker.totalCpu;
+        });
+    }
+}
+
+function printDetailedStatus(app) {
+	var tableHeader = ["", "PID", "Ports", "Uptime", "CPU", "MEM", "Processes"];
+	tableHeader = tableHeader.map(function(header){
+		return header.cyan.bold;
 	});
 
+	var table = new Table({head: tableHeader});
+	var row = {};
+    var uptime = moment.duration(app.status.uptime, "seconds").humanize();
+    makeTotals(app);
+
+	row[app.id.bold] = [app.status.pid, "--", uptime, app.status.totalCpu.toFixed(2) + "%", prettyMem(app.status.totalMem), app.status.totalProc];
+	table.push(row);
+	_.each(app.status.workers, function(worker){
+		var row = {};
+        var uptime = moment.duration(worker.uptime, "seconds").humanize();
+        
+		row[(app.id + ":" + worker.id).bold] = [worker.pid, _.pluck(worker.listening, "port"), uptime, prettyMem(worker.totalMem), worker.totalCpu.toFixed(2) + "%", worker.totalProc]
+		table.push(row);
+	});
 	console.log(table.toString());
 }
